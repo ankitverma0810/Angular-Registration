af24a96dd6065463742346befbe7c5a2dbf73d61
ankit <ankitverma_0810@yahoo.co.in>
2016-02-01 12:27:41 +0530

    Updated meeting controller

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,2 +1,28 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
+myApp.controller('MeetingsController', ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',  function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+
+	var ref = new Firebase(FIREBASE_URL);
+	var auth = $firebaseAuth(ref);
+
+	auth.$onAuth(function(authUser) {
+		if(authUser) {
+			//console.log($rootScope.currentUser.$id);
+			var meetingRef = new Firebase(FIREBASE_URL+'users/'+$rootScope.currentUser.$id+'/meetings');
+			var meetingsInfo = $firebaseArray(meetingRef);
+
+			$scope.meetings = meetingsInfo;
+
+			$scope.addMeeting = function() {
+				meetingsInfo.$add({
+					name: $scope.meetingname,
+					date: Firebase.ServerValue.TIMESTAMP
+				}).then(function() {
+					$scope.meetingname = '';
+				});
+			};
+
+			$scope.deleteMeeting = function(key) {
+				meetingsInfo.$remove(key);
+			};
+		}
+	});
 }]);
